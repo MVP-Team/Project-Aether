690929cecd9ff098fed837402a6e387c5c1b7696
BogiPre <106031429+BogiPre@users.noreply.github.com>
2022-09-30 22:03:18 +0200

    Revert "Changed avalonia app to MVVM"

--- Aether/App.axaml.cs
+++ Aether/App.axaml.cs
@@ -1,29 +1,24 @@
-using Aether.ViewModels;
-using Aether.Views;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace Aether
 {
-	public partial class App : Application
-	{
-		public override void Initialize()
-		{
-			AvaloniaXamlLoader.Load(this);
-		}
+    public partial class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
 
-		public override void OnFrameworkInitializationCompleted()
-		{
-			if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-			{
-				desktop.MainWindow = new MainWindow
-				{
-					DataContext = new MainWindowViewModel(),
-				};
-			}
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.MainWindow = new MainWindow();
+            }
 
-			base.OnFrameworkInitializationCompleted();
-		}
-	}
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
 }
