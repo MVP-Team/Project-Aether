86278499ef2320aacac991fd1df0c64a6e9ecd1d
Alexander-Hac <hac22567@spengergasse.at>
2022-09-30 20:35:30 +0200

    New Change

--- Aether-Console/Program.cs
+++ Aether-Console/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Aether_Console.Terminal;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 Basis.Lines();
