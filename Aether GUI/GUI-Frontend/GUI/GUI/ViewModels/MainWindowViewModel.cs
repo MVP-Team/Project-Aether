86c22760d7f58ca838650cabea080a6deaa37bb6
Mutantboy <philipp.khachik@gmail.com>
2022-12-01 20:51:13 +0100

    Erweiterte Button Funktion, programmiert am 8.11

--- Aether GUI/GUI-Frontend/GUI/GUI/ViewModels/MainWindowViewModel.cs
+++ Aether GUI/GUI-Frontend/GUI/GUI/ViewModels/MainWindowViewModel.cs
@@ -21,13 +21,30 @@ namespace GUI.ViewModels
             set => this.RaiseAndSetIfChanged(ref _txtBoxVisibility, value);
         }
 
+        private bool _aetherWelcomeVisibility = true;
+
+        public bool AetherWelcomeVisibility
+        {
+            get => _aetherWelcomeVisibility;
+            set => this.RaiseAndSetIfChanged(ref _aetherWelcomeVisibility, value);
+        }
+
 
         //Eigene var
         private bool btnState = false;
         public void EnableTxtInput()
         {
             btnState = !btnState;
+
             TxtBoxVisibility = btnState;
+            AetherWelcomeVisibility = !btnState;
+        }
+
+        private bool expanderState = true;
+        public void ExpanderClick()
+        {
+            AetherWelcomeVisibility = !expanderState;
+
         }
     }
 }
